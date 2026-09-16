1c4921dbbf2fa81b3ee20eaaa3a0dd7986ab9ee3
Leonardo Hidifira <yuitileonardo@gmail.com>
2026-09-16 20:10:01 -0300

    exercicio de dicionario com pokemon

---
+++ main.py
@@ -0,0 +1,15 @@
+# Faça uma função para extrair o tipo e o id do pokémon na pokedex dado um nome de um pokemon
+
+import requests
+
+def tipo_poke(pokemon):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
+    r = requests.get(url)
+    info = r.json()
+    id = info['id']
+    tipo = []
+    for i in range(len(info['types'])):
+        tipo.append(info['types'][i]['type']['name'])
+    return id, tipo
+
+print(tipo_poke('charizard'))
